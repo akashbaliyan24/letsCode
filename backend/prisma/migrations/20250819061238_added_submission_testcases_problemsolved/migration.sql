df9fb31e09f4ba176311535fc1e6788eebcde9aa
Akash <akashbaliya24@gmail.com>
2025-08-20 16:10:58 +0530

    new data added inside migrations

---
+++ backend/prisma/migrations/20250819061238_added_submission_testcases_problemsolved/migration.sql
@@ -0,0 +1,70 @@
+-- CreateTable
+CREATE TABLE "public"."Submission" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "problemId" TEXT NOT NULL,
+    "sourceCode" JSONB NOT NULL,
+    "language" TEXT NOT NULL,
+    "stdin" TEXT,
+    "stdout" TEXT,
+    "stderr" TEXT,
+    "compileOutput" TEXT,
+    "status" TEXT NOT NULL,
+    "memory" TEXT,
+    "time" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."testCaseResult" (
+    "id" TEXT NOT NULL,
+    "submissionId" TEXT NOT NULL,
+    "testCase" INTEGER NOT NULL,
+    "passed" BOOLEAN NOT NULL,
+    "stdout" TEXT,
+    "expected" TEXT NOT NULL,
+    "stderr" TEXT,
+    "compileOutput" TEXT,
+    "status" TEXT NOT NULL,
+    "memory" TEXT,
+    "time" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "testCaseResult_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."problemSolved" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "problemId" TEXT NOT NULL,
+    "cretedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "problemSolved_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "testCaseResult_submissionId_idx" ON "public"."testCaseResult"("submissionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "problemSolved_userId_problemId_key" ON "public"."problemSolved"("userId", "problemId");
+
+-- AddForeignKey
+ALTER TABLE "public"."Submission" ADD CONSTRAINT "Submission_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Submission" ADD CONSTRAINT "Submission_problemId_fkey" FOREIGN KEY ("problemId") REFERENCES "public"."Problem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."testCaseResult" ADD CONSTRAINT "testCaseResult_submissionId_fkey" FOREIGN KEY ("submissionId") REFERENCES "public"."Submission"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."problemSolved" ADD CONSTRAINT "problemSolved_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."problemSolved" ADD CONSTRAINT "problemSolved_problemId_fkey" FOREIGN KEY ("problemId") REFERENCES "public"."Problem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
