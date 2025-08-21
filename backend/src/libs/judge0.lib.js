c5fe46879474369841bf3786c80d325de50ade0c
Akash <akashbaliya24@gmail.com>
2025-08-21 11:32:31 +0530

    add new route inside index.js and add language name logic inside judge0.lib.js

--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -1,72 +1,90 @@
 import axios from "axios"
-export const getJudge0LanguageId = (Language) =>{
+export const getJudge0LanguageId = (Language) => {
 
-    if(typeof Language !== "string") return undefined;
-    const aliasMap ={
-        "PY" : "PYTHON",
-        "PYTHON":"PYTHON",
-        "JS" : "JAVASCRIPT",
-        "JAVASCRIPT" : "JAVASCRIPT",
-        "TS" : "TYPESCRIPT",
-        "TYPESCRIPT" : "TYPESCRIPT",
-        "CPP" : "C++",
-        "C++" : "C++",
+    if (typeof Language !== "string") return undefined;
+    const aliasMap = {
+        "PY": "PYTHON",
+        "PYTHON": "PYTHON",
+        "JS": "JAVASCRIPT",
+        "JAVASCRIPT": "JAVASCRIPT",
+        "TS": "TYPESCRIPT",
+        "TYPESCRIPT": "TYPESCRIPT",
+        "CPP": "C++",
+        "C++": "C++",
         "CS": "C#",
-        "C#" : "C#",
-        "C" : "C",
-        "JAVA" : "JAVA",
-        "KOTLIN" : "KOTLIN",
-        "KT" : "KOTLIN",
-        "PHP" : "PHP",
-        "RB" : "RUBY",
-        "RUBY" : "RUBY",
-        "R" : "R",
-        "RUST" : "RUST",
+        "C#": "C#",
+        "C": "C",
+        "JAVA": "JAVA",
+        "KOTLIN": "KOTLIN",
+        "KT": "KOTLIN",
+        "PHP": "PHP",
+        "RB": "RUBY",
+        "RUBY": "RUBY",
+        "R": "R",
+        "RUST": "RUST",
     }
     const languageMap = {
-        "C" : 75,
-        "C++" : 76,
-        "PYTHON" : 71,
-        "JAVA" : 62,
-        "JAVASCRIPT" : 63,
-        "C#" : 51,
-        "KOTLIN" : 78,
-        "PHP" : 68,
-        "R" : 80,
-        "RUBY" : 72,
-        "RUST":73,
-        "TYPESCRIPT":74
+        "C": 75,
+        "C++": 76,
+        "PYTHON": 71,
+        "JAVA": 62,
+        "JAVASCRIPT": 63,
+        "C#": 51,
+        "KOTLIN": 78,
+        "PHP": 68,
+        "R": 80,
+        "RUBY": 72,
+        "RUST": 73,
+        "TYPESCRIPT": 74
     }
 
     const key = aliasMap[Language.trim().toUpperCase()]
-    return languageMap[key] 
+    return languageMap[key]
 }
-const sleep = (ms) => new Promise((resolve)=> setTimeout(resolve,ms))
-export const pollBatchResults = async (tokens)=>{
-    while(true){
-        const {data} = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`,{
-            params :{
-                tokens : tokens.join(","),
-                base64_encoded:false,
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+export const pollBatchResults = async (tokens) => {
+    while (true) {
+        const { data } = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`, {
+            params: {
+                tokens: tokens.join(","),
+                base64_encoded: false,
             }
         })
 
         const results = data.submissions
 
-        const isAllDone = results.every((r)=> r.status.id !== 1 && r.status.id !==2)
+        const isAllDone = results.every((r) => r.status.id !== 1 && r.status.id !== 2)
 
-        if(isAllDone) return results
+        if (isAllDone) return results
 
         await sleep(1000)
     }
 }
 
-export const submitBatch = async (submissions)=>{
-    const {data} = await axios.post(`${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`,{
+export const submitBatch = async (submissions) => {
+    const { data } = await axios.post(`${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`, {
         submissions
     })
 
-    console.log("Submission Result:" ,data);
+    console.log("Submission Result:", data);
 
     return data //[{token}]
+}
+
+export const getLanguageName = (language_id) => {
+    const LANGUAGE_NAMES = {
+        75: "C",
+        76: "C++",
+        71: "PYTHON",
+        62: "JAVA",
+        63: "JAVASCRIPT",
+        51: "C#",
+        78: "KOTLIN",
+        68: "PHP",
+        80: "R",
+        72: "RUBY",
+        73: "RUST",
+        74: "TYPESCRIPT"
+    }
+    return LANGUAGE_NAMES[language_id] || "UNKNOWN";
 }
