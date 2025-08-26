ed6a9ca3b06becce5e5f07d8998e559c92630be2
Akash <akashbaliya24@gmail.com>
2025-08-26 10:49:29 +0530

    Add cors in server for fetching data from the front-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,21 +6,28 @@ import problemRoutes from "./routes/problem.routes.js";
 import executionRoute from "./routes/executeCode.routes.js";
 import submissionRoutes from "./routes/submission.routes.js";
 import playlistRoutes from "./routes/playlist.routes.js";
+import cors from "cors"
 dotenv.config();
 const app = express();
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    // methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 app.use(express.json())
 app.use(cookieParser())
-const port = process.env.PORT 
 
-app.get("/",(req,res)=>{
+const port = process.env.PORT
+
+app.get("/", (req, res) => {
     res.send("hello guys welcome to leetlab🪄")
 })
 
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/problems",problemRoutes)
-app.use("/api/v1/execute-code",executionRoute)
-app.use("/api/v1/submission",submissionRoutes)
-app.use("/api/v1/playlist",playlistRoutes)
-app.listen(port,()=>{
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes)
+app.use("/api/v1/execute-code", executionRoute)
+app.use("/api/v1/submission", submissionRoutes)
+app.use("/api/v1/playlist", playlistRoutes)
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
