aa4c1fd46ff33f40a3d8d334e271b5445fd3f290
Akash <akashbaliya24@gmail.com>
2025-09-04 13:03:19 +0530

    Add store for getting problem and solution data from backend

---
+++ frontend/src/store/useProblemStore.js
@@ -0,0 +1,58 @@
+import { create } from "zustand"
+import { axiosInstace } from "../lib/axios.js"
+
+import { toast } from "react-hot-toast"
+
+export const useProblemStore = create((set) => ({
+    problems: [],
+    problem: null,
+    solvedProblems: [],
+    isProblemsLoading: false,
+    isProblemLoading: false,
+
+
+    getAllProblem: async () => {
+        try {
+            set({ isProblemsLoading: true })
+            const res = await axiosInstace.get("/problems/get-all-problems");
+
+            set({ problems: res.data.problems })
+
+        } catch (error) {
+            console.log("error getting all problems", error);
+            toast.error("Error in gee=tting Problems")
+        }
+        finally {
+            set({ isProblemsLoading: false })
+        }
+    },
+
+    getProblemById: async (id) => {
+        try {
+            set({ isProblemLoading: true })
+
+            const res = await axiosInstace.get(`/problems/get-problem/${id}`)
+
+            set({ problem: res.data.problem })
+            toast.success(res.data.problem)
+        } catch (error) {
+            console.log("Error in getting problems")
+            toast.error("Error in getting problem")
+        }
+
+        finally {
+            set({ isProblemLoading: false })
+        }
+    },
+
+    getSolvedProblemByUser: async () => {
+        try {
+            const res = await axiosInstace.get("/problems/get-solved-probelem")
+            set({ solvedProblems: res.data.problems })
+        } catch (error) {
+            console.log("Error in getting solved problems", Error)
+            toast.error("Error in getting solved problems")
+        }
+    }
+}))
+
