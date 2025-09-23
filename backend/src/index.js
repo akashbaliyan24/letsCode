20bf10611e99e7e5e5262f7bba65f25ad13ec648
Akash <akashbaliya24@gmail.com>
2025-09-23 17:54:00 +0530

    update in index.js for frontend url

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,11 +9,16 @@ import playlistRoutes from "./routes/playlist.routes.js";
 import cors from "cors"
 dotenv.config();
 const app = express();
+const allowedOrigins = process.env.FRONTEND_URLS.split(",");
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    // methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) callback(null, true);
+        else callback(new Error("Not allowed by CORS"));
+    },
     credentials: true
-}))
+}));
+
 app.use(express.json())
 app.use(cookieParser())
 
