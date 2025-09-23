3269049e7cf1ae1df876c0fe1e45b9d32a0ddab3
Akash <akashbaliya24@gmail.com>
2025-09-23 22:17:29 +0530

    add base url as env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,14 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
-    withCredentials : true,
+    baseURL : import.meta.env.VITE_API_BASE_URL,
+    withCredentials: true,
 })
-
-// axiosInstance.interceptors.request.use((config) =>{
-//     const token = localStorage.getItem("token")
-//     if(token){
-//         config.headers.Authorization = `Bearer ${token}`
-//     }
-//     return config;
-// })
