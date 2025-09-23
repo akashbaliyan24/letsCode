97d4c03cf284060c028c7b3de0ba0c536a277b1c
Akash <akashbaliya24@gmail.com>
2025-09-23 22:07:30 +0530

    add cors for env files for production and development

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,42 @@
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from 'cookie-parser'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+
 import authRoutes from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problem.routes.js";
 import executionRoute from "./routes/executeCode.routes.js";
 import submissionRoutes from "./routes/submission.routes.js";
 import playlistRoutes from "./routes/playlist.routes.js";
-import cors from "cors"
-dotenv.config();
+
+// Load environment variables
+const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
+dotenv.config({ path: envFile });
+
 const app = express();
+
+// CORS
 app.use(cors({
     origin: process.env.FRONTEND_URL,
-    // methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
-}))
-app.use(express.json())
-app.use(cookieParser())
+}));
 
-const port = process.env.PORT
+app.use(express.json());
+app.use(cookieParser());
+
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    res.send("hello guys welcome to leetlab🪄")
-})
+    res.send("Hello guys, welcome to Leetlab🪄");
+});
 
+// Routes
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/problems", problemRoutes)
-app.use("/api/v1/execute-code", executionRoute)
-app.use("/api/v1/submission", submissionRoutes)
-app.use("/api/v1/playlist", playlistRoutes)
+app.use("/api/v1/problems", problemRoutes);
+app.use("/api/v1/execute-code", executionRoute);
+app.use("/api/v1/submission", submissionRoutes);
+app.use("/api/v1/playlist", playlistRoutes);
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
